fd3ccb7efe64da40e557228ca50d354858d84736
Nikita Shpulin <shpulinn@mail.ru>
2025-10-07 10:55:01 +0200

    basic NetworkManagerCustom

---
+++ Assets/Scripts/NetworkManagerCustom.cs
@@ -0,0 +1,13 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class NetworkManagerCustom : NetworkManager
+{
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        var player = Instantiate(playerPrefab);
+        NetworkServer.AddPlayerForConnection(conn, player);
+
+        Debug.Log($"Player connected: {conn.connectionId}");
+    }
+}
