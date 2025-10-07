0a056f104bbd1259f9f4d6294886f60bd11c38ca
Nikita Shpulin <shpulinn@mail.ru>
2025-10-07 12:39:48 +0200

    NicknameService created

---
+++ Assets/Scripts/NicknameService.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class NicknameService
+{
+    public string Nickname { get; private set; }
+
+    public void SetNickname(string nickname)
+    {
+        Nickname = nickname;
+        Debug.Log($"Nickname set: {nickname}");
+    }
+}
