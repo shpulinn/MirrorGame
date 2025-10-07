e98b98e0b759520f3acbbdb4501597b292db8602
Nikita Shpulin <shpulinn@mail.ru>
2025-10-07 10:32:20 +0200

    PlayerInput class

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerInput
+{
+    public float MoveInput { get; private set; }
+    public float RotateInput { get; private set; }
+    public bool SendMessagePressed { get; private set; }
+
+    public void Update()
+    {
+        MoveInput = Input.GetAxis("Vertical");
+        RotateInput = Input.GetAxis("Horizontal");
+        SendMessagePressed = Input.GetKeyDown(KeyCode.Space);
+    }
+
+    public void ResetOneFrameFlags()
+    {
+        SendMessagePressed = false;
+    }
+}
