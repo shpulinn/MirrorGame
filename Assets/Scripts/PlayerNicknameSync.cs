08734abede1e80052ce2b0a937093a5e985170ac
Nikita Shpulin <shpulinn@mail.ru>
2025-10-08 12:29:23 +0200

    PlayerNicknameSync class created

---
+++ Assets/Scripts/PlayerNicknameSync.cs
@@ -0,0 +1,23 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class PlayerNicknameSync : NetworkBehaviour
+{
+    [SyncVar(hook = nameof(OnNicknameChanged))]
+    public string nickname = "Player";
+
+    private void OnNicknameChanged(string oldNickname, string newNickname)
+    {
+        Debug.Log($"Никнейм изменен: {oldNickname} -> {newNickname}");
+        UpdateNicknameDisplay();
+    }
+
+    private void UpdateNicknameDisplay()
+    {
+        var nameText = GetComponentInChildren<TMPro.TextMeshProUGUI>();
+        if (nameText != null)
+        {
+            nameText.text = nickname;
+        }
+    }
+}
