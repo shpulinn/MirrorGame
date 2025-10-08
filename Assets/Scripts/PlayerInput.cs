c1d7153181a8d0171001c5cfd72109edca163c29
Nikita Shpulin <shpulinn@mail.ru>
2025-10-08 17:34:41 +0200

    Update PlayerInput.cs
    new input

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,16 +5,20 @@ public class PlayerInput
     public float MoveInput { get; private set; }
     public float RotateInput { get; private set; }
     public bool SendMessagePressed { get; private set; }
+    
+    public bool SpawnPressed { get; private set; }
 
     public void Update()
     {
         MoveInput = Input.GetAxis("Vertical");
         RotateInput = Input.GetAxis("Horizontal");
         SendMessagePressed = Input.GetKeyDown(KeyCode.Space);
+        SpawnPressed = Input.GetKeyDown(KeyCode.F);
     }
 
     public void ResetOneFrameFlags()
     {
         SendMessagePressed = false;
+        SpawnPressed = false;
     }
 }
