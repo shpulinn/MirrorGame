f56291eac1a12dd55afd35f90024cfcbc8df7fb7
Nikita Shpulin <shpulinn@mail.ru>
2025-10-08 12:13:10 +0200

    Update PlayerScope.cs
    unnecessary registrations have been removed

--- Assets/Scripts/PlayerScope.cs
+++ Assets/Scripts/PlayerScope.cs
@@ -5,8 +5,6 @@ public class PlayerScope : LifetimeScope
 {
     protected override void Configure(IContainerBuilder builder)
     {
-        builder.Register<PlayerInput>(Lifetime.Singleton);
-        builder.Register<NicknameService>(Lifetime.Singleton);
-        builder.RegisterComponentInHierarchy<PlayerController>();
+        builder.Register<PlayerInput>(Lifetime.Scoped);
     }
 }
