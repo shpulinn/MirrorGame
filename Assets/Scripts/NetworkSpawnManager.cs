7e1be5fb204c2329ba2bd767c771b240598b7dfa
Nikita Shpulin <shpulinn@mail.ru>
2025-10-08 17:34:19 +0200

    NetworkSpawnManager created

---
+++ Assets/Scripts/NetworkSpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class NetworkSpawnManager : NetworkBehaviour
+{
+    public static NetworkSpawnManager Instance;
+
+    [SerializeField] private GameObject cubePrefab;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    [Server]
+    public void SpawnCube(Vector3 position, Quaternion rotation)
+    {
+        var cube = Instantiate(cubePrefab, position, rotation);
+        NetworkServer.Spawn(cube);
+    }
+}
